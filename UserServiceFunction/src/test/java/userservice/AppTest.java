05b06d374d4d6c7f911f41dc4a39727a3329e3a2
kcwong395 <kcwong395@gmail.com>
2025-04-23 00:48:23 -0400

    init project

---
+++ UserServiceFunction/src/test/java/userservice/AppTest.java
@@ -0,0 +1,13 @@
+package userservice;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+  @Test
+  public void successfulResponse() {
+  }
+}
